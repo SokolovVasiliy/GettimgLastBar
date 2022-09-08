60322ff062ab8c341fab13006bbd5b694aebb5ea
Василий Соколов <bazillive@live.ru>
2022-09-08 13:37:15 +0300

    Add project files.

---
+++ SignalGenerator.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace VeeTrader.Others
+{
+    /// <summary>
+    /// Define period of timer event
+    /// </summary>
+    public enum SignalGeneratorAlign
+    {
+        /// <summary>
+        /// Milliseconds period 
+        /// </summary>
+        Milliseconds,
+        /// <summary>
+        /// Seconds period
+        /// </summary>
+        Seconds,
+        /// <summary>
+        /// Minutes period
+        /// </summary>
+        Minutes,
+        /// <summary>
+        /// Hours period
+        /// </summary>
+        Hours
+    }
+    /// <summary>
+    /// Generate new signal with specified intervals aligned by time borders
+    /// </summary>
+    public class SignalGenerator
+    {
+        /// <summary>
+        /// List of clients
+        /// </summary>
+        Dictionary<string, Action> m_subscrible_list = new Dictionary<string, Action>();
+        /// <summary>
+        /// Subscrible count
+        /// </summary>
+        private int m_subscrible_count = 0;
+        /// <summary>
+        /// Synch obj
+        /// </summary>
+        private object m_state = new object();
+        /// <summary>
+        /// Timer
+        /// </summary>
+        private Timer m_timer;
+        /// <summary>
+        /// TimeSpan as align by time border
+        /// </summary>
+        private TimeSpan m_align;
+        /// <summary>
+        /// Time delay
+        /// </summary>
+        private TimeSpan m_time_delay;
+        /// <summary>
+        /// Количество шагов
+        /// </summary>
+        private long m_prev_steps = 0;
+        /// <summary>
+        /// Milliseconds elapsed between two call timer
+        /// </summary>
+        private long m_max_elapsed = 0;
+        /// <summary>
+        /// Stopwatch for exact time calculate
+        /// </summary>
+        private Stopwatch m_stopwatch = new Stopwatch();
+
+        /// <summary>
+        /// Prohibit premature triggering
+        /// </summary>
+        public bool UseStrongMoreTime { get; set; }
+        /// <summary>
+        /// Create signal generator with indicating the frequency of operation
+        /// </summary>
+        /// <param name="align">Align of time</param>
+        /// <param name="period">Period of event</param>
+        public SignalGenerator(SignalGeneratorAlign align, int period) : this(align, period, 20, false)
+        {
+        }
+        /// <summary>
+        /// Create signal generator with indicating the frequency of operation
+        /// </summary>
+        /// <param name="align">Align of time</param>
+        /// <param name="period">Period of event</param>
+        /// <param name="maxResolution">Maximum resolution of the timer in milliseconds. By default it equal 20 milliseconds. Don't change this value if undestund what do you do</param>
+        public SignalGenerator(SignalGeneratorAlign align, int period, int maxResolution = 20, bool useStrongMoreTime = false)
+        {
+            //-- Set TimeSpan as time period
+            UseStrongMoreTime = useStrongMoreTime;
+            var align_to_time_span = new Dictionary<SignalGeneratorAlign, Func<int, TimeSpan>>
+            {
+                { SignalGeneratorAlign.Milliseconds, (count) => new TimeSpan(0, 0, 0, 0, count) },
+                { SignalGeneratorAlign.Seconds, (count) => new TimeSpan(0, 0, count) },
+                { SignalGeneratorAlign.Minutes, (count) => new TimeSpan(0, count, 0) },
+                { SignalGeneratorAlign.Hours, (count) => new TimeSpan(count, 0, 0) },
+            };
+            m_align = align_to_time_span[align](period);
+            m_time_delay = new TimeSpan(0, 0, 0, 0, maxResolution);
+            m_timer = new Timer(OnTimer, m_state, m_time_delay.Milliseconds, m_time_delay.Milliseconds);
+        }
+        /// <summary>
+        /// High-frequncy timer
+        /// </summary>
+        /// <param name="state"></param>
+        private void OnTimer(object state)
+        {
+            try
+            {
+                Monitor.Enter(state);
+                if (m_stopwatch.ElapsedMilliseconds > m_max_elapsed)
+                    m_max_elapsed = m_stopwatch.ElapsedMilliseconds;
+                DateTime time = DateTime.Now;
+                long steps_next;
+                if (!UseStrongMoreTime)
+                    steps_next = (time + m_time_delay + m_time_delay).Ticks / m_align.Ticks;
+                else
+                    steps_next = time.Ticks / m_align.Ticks;
+                if (steps_next > m_prev_steps)
+                {
+                    EventNewSignal?.Invoke();
+                    m_prev_steps = steps_next;
+                    m_max_elapsed = 0;
+                }
+                m_stopwatch.Restart();
+            }
+            catch(Exception ex)
+            {
+                ;
+            }
+            finally
+            {
+                Monitor.Exit(state);
+            }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        public void SubscribleOnSignal(string uniqKey, Action onSignal)
+        {
+            Action oldSubClient;
+            //lock (m_subscrible_list)
+            {
+                if (!m_subscrible_list.TryGetValue(uniqKey, out oldSubClient))
+                {
+                    EventNewSignal += onSignal;
+                    m_subscrible_list.Add(uniqKey, onSignal);
+                    m_subscrible_count++;
+                }
+                else
+                {
+                    EventNewSignal -= oldSubClient;
+                    EventNewSignal += onSignal;
+                    m_subscrible_list[uniqKey] = onSignal;
+                }
+            }
+        }
+        /// <summary>
+        /// Return current subscrible method
+        /// </summary>
+        public Action GetCurrentSubsribleMethod(string uniqKey)
+        {
+            Action currentMethod;
+            m_subscrible_list.TryGetValue(uniqKey, out currentMethod);
+            return currentMethod;
+        }
+        /// <summary>
+        /// Unsubscrible client with uniq name from current signal
+        /// </summary>
+        public bool UnSubscribleOnSignal(string uniqKey)
+        {
+            Action oldSubClient;
+            if (m_subscrible_list.TryGetValue(uniqKey, out oldSubClient))
+            {
+                EventNewSignal -= oldSubClient;
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// New signal action
+        /// </summary>
+        public event Action EventNewSignal;
+    }
+}
---
+++ TrendQuoting2.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using TSLab.Script.Handlers;
+using TSLab.Script.Realtime;
+using TSLab.DataSource;
+using TSLab.ScriptExecution.Realtime;
+using VeeTrader.Others;
+using System.Text;
+using TSLab.Script;
+
+/// <summary>
+/// Disable obsolete warning
+/// </summary>
+#pragma warning disable CS0612
+// Disable integrated variable
+#pragma warning disable IDE0018
+namespace TSLab.TestAddons
+{
+    /// <summary>
+    /// Получатель сигнала через WebSocket
+    /// </summary>
+    [HandlerName("GettingLastBarTest")]
+    [HandlerCategory("Testing")]
+    public class TrendQuoting2 : BasePeriodIndicatorHandler, IContextUses, IBar2ValueDoubleHandler, IDisposable
+    {
+        Random m_rnd = new Random();
+        /// <summary>
+        /// Signal generator
+        /// </summary>
+        private static readonly SignalGenerator m_sig_gen = new SignalGenerator(SignalGeneratorAlign.Minutes, 1, 20, true);
+        /// <summary>
+        /// Запомненный последний рабочий символ для таймера
+        /// </summary>
+        ISecurityRt m_secRt;
+        /// <summary>
+        /// Current context
+        /// </summary>
+        public IContext Context { get; set; }
+        /// <summary>
+        /// Qty of each deal
+        /// </summary>
+        [HandlerParameter(Default = "true", NotOptimized = true)]
+        [Description("Истина, если отклонение задается в процентах от 0 до 100 от текущей цены. Ложь - отклонение задается в пунктах.")]
+        public bool UseDeviationPercent { get; set; }
+        /// <summary>
+        /// Connect string
+        /// </summary>
+        string m_connect_string = "tcp://gtam";
+        /// <summary>
+        /// Рудементарный параметр, используемый для инициализации таймера, больше никак
+        /// </summary>
+        [HandlerParameter(Default = "tcp://gtam", NotOptimized = true)]
+        public string ConnectString
+        {
+            get
+            {
+                return m_connect_string;
+            }
+            set
+            {
+                m_connect_string = value;
+                m_sig_gen.EventNewSignal += OnTimer;
+            }
+        }
+        /// <summary>
+        /// Unsubscrible from get timer
+        /// </summary>
+        public void Dispose()
+        {
+            m_sig_gen.EventNewSignal -= OnTimer;
+        }
+        /// <summary>
+        /// Main execute module
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="barNum"></param>
+        /// <returns></returns>
+        public double Execute(ISecurity source, int barNum)
+        {
+            try
+            {
+                if (barNum >= source.Bars.Count)
+                    return 0.0;
+                if (IsRealTime(barNum))
+                    ExecuteRealTime(source);
+                int sign = m_rnd.Next(0, 1) == 0 ? -1 : 1;
+                return sign;
+            }
+            catch (Exception ex)
+            {
+                StringBuilder info = new StringBuilder();
+                info.Append("Получено необработанное исключение в обработчике SignalTakerInstance.Execute: ");
+                info.Append(ex.Message).Append("\nStackTrace:\n");
+                info.Append(ex.StackTrace);
+                Context.Log(info.ToString(), new Color(), true);
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Return true if trading is realtime, otherwise return false
+        /// </summary>
+        /// <param name="barNum">Number current bar</param>
+        /// <returns></returns>
+        private bool IsRealTime(int barNum)
+        {
+            int barsCount = Context.BarsCount;
+            if (!Context.IsLastBarUsed)
+                barsCount--;
+            if (barNum < barsCount - 1)
+                return false;
+            return true;
+        }
+        /// <summary>
+        /// Execute on RealTime
+        /// </summary>
+        /// <param name="source"></param>
+        private void ExecuteRealTime(ISecurity source)
+        {
+            m_secRt = source as ISecurityRt;
+            //OnBuyState(m_secRt);
+            //OnSellState(m_secRt);
+        }
+        /// <summary>
+        /// Срабатывает раз в минуту, на границе минутного таймфрейма (что отличается его от обычного минутного таймера)
+        /// </summary>
+        protected void OnTimer()
+        {
+            try
+            {
+                DateTime timeUpdateBegin = DateTime.Now;
+                if (m_secRt == null)
+                    Context.Log("ISecurityRt is null. Perhaps method 'Excecute' wasn't be called", new Color(), true);
+                IReadOnlyList<IDataBar> bars = m_secRt.Bars;
+                IDataBar bar = bars[bars.Count - 1];
+                TimeSpan delta = DateTime.UtcNow - bar.Date;
+                Context.Log($"На момент {DateTime.UtcNow.ToString("HH:mm:ss")} получен бар с временем {bar.Date.ToString("HH:mm:ss")}. Разница {delta}");
+                //-- заставляем пересчитаться агента на границе бара
+                Context?.Runtime.Recalc(true);
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+
+        /// <summary>
+        /// Return trade allowed
+        /// </summary>
+        /// <param name="secRt"></param>
+        /// <returns></returns>
+        private bool IsTradeAllowed(ISecurityRt secRt)
+        {
+            try
+            {
+                RtSecurityImpl rt = (RtSecurityImpl)secRt;
+                bool a = rt.Security.TradePlace.AllowTrade;
+                return a;
+            }
+            catch
+            {
+                return true;
+            }
+        }
+        
+        
+        /// <summary>
+        /// Buy logic
+        /// </summary>
+        /// <param name="secRt"></param>
+        private void OnBuyState(ISecurityRt secRt)
+        {
+            
+        }
+        /// <summary>
+        /// Sell logic
+        /// </summary>
+        /// <param name="secRt"></param>
+        private void OnSellState(ISecurityRt secRt)
+        {
+            
+        }
+        
+        
+    }
+}
